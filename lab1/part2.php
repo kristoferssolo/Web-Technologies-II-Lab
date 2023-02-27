08b98bf186e5185841db769fbabc4085bed53794
Kristofers Solo <kristians.cagulis@proton.me>
2023-02-27 19:41:44 +0200

    Part 2 task 6

--- lab1/part2.php
+++ lab1/part2.php
@@ -21,6 +21,14 @@ function get_first_monday($year) {
     }
 }
 
+function ordinal($number) {
+    $ends = ["th","st","nd","rd","th","th","th","th","th","th"];
+    if ((($number % 100) >= 11) && (($number%100) <= 13))
+        return $number. "th";
+    else
+        return $number. $ends[$number % 10];
+}
+
 print_alphabet();
 echo "\n";
 
@@ -30,5 +38,12 @@ assert_options(date("d-m-Y", get_first_monday(2021))=="04-01-2021");
 echo date("d-m-Y", get_first_monday(2023))."\n";
 echo date("d-m-Y", get_first_monday(2022))."\n";
 echo date("d-m-Y", get_first_monday(2021))."\n";
+
+$years = [2023,2030,2016,2017];
+foreach ($years as $year) {
+    $date = ordinal((int)date('d', get_first_monday($year)));
+    echo "The first Monday of year {$year} is {$date} January\n";
+}
+
 ?>   
 
