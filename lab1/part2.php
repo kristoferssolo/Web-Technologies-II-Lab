87d3b04b85952dec7e7372c14b136c07328a08cd
Kristofers Solo <kristians.cagulis@proton.me>
2023-02-27 19:34:06 +0200

    Part 2 task 5

--- lab1/part2.php
+++ lab1/part2.php
@@ -3,25 +3,32 @@ header("Content-Type: text/plain");
 
 
 function print_alphabet() {
-    $SEPARATOR = "\n";
-    echo "Part 2, subtask1{$SEPARATOR}";
+    echo "Part 2, subtask1\n";
     $letters = ["a", "ā", "b", "c", "č", "d", "e", "ē", "f", "g", "ģ", "h", "i", "ī", "j", "k", "ķ", "l", "ļ", "m", "n", "ņ", "o", "p", "r", "s", "š","t","u","ū","v","z","ž"];
 
     $string= "";
     foreach ($letters as $letter) {
         $string .= $letter;
-        echo "{$string}{$SEPARATOR}";
+        echo "{$string}\n";
     }
 }
 
 function get_first_monday($year) {
-    return $year;
+    for ($day=1; $day<=7 ;$day++){
+        $date = mktime(0,0,0,1,$day,$year);
+        if (date("D", $date) === "Mon")
+            return $date;
+    }
 }
 
 print_alphabet();
+echo "\n";
 
-echo get_first_monday(2023);
-
-
-?>
+assert_options(date("d-m-Y", get_first_monday(2023))=="02-01-2023");
+assert_options(date("d-m-Y", get_first_monday(2022))=="03-01-2022");
+assert_options(date("d-m-Y", get_first_monday(2021))=="04-01-2021");
+echo date("d-m-Y", get_first_monday(2023))."\n";
+echo date("d-m-Y", get_first_monday(2022))."\n";
+echo date("d-m-Y", get_first_monday(2021))."\n";
+?>   
 
