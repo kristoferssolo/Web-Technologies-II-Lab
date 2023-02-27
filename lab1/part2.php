a7c4a28f72dd9b1cd1a80fea4c8cbec7f797ba42
Kristofers Solo <kristians.cagulis@proton.me>
2023-02-27 19:06:08 +0200

    Part 2 task 4

--- lab1/part2.php
+++ lab1/part2.php
@@ -1,3 +1,27 @@
 <?php
 header("Content-Type: text/plain");
+
+
+function print_alphabet() {
+    $SEPARATOR = "\n";
+    echo "Part 2, subtask1{$SEPARATOR}";
+    $letters = ["a", "ā", "b", "c", "č", "d", "e", "ē", "f", "g", "ģ", "h", "i", "ī", "j", "k", "ķ", "l", "ļ", "m", "n", "ņ", "o", "p", "r", "s", "š","t","u","ū","v","z","ž"];
+
+    $string= "";
+    foreach ($letters as $letter) {
+        $string .= $letter;
+        echo "{$string}{$SEPARATOR}";
+    }
+}
+
+function get_first_monday($year) {
+    return $year;
+}
+
+print_alphabet();
+
+echo get_first_monday(2023);
+
+
 ?>
+
