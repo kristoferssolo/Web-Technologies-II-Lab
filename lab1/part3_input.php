537cdd5ae1faa4a3fe2eee96fa8eb92f5d14daeb
Kristofers Solo <kristians.cagulis@proton.me>
2023-02-27 20:17:58 +0200

    Part 3 finished

--- lab1/part3_input.php
+++ lab1/part3_input.php
@@ -1,8 +1,36 @@
 <?php
 header("Content-Type: text/plain");
-$start = $_GET["start"];
-$end = $_GET["end"];
+$start = (int)$_GET["start"];
+$end = (int)$_GET["end"];
 
-echo $start . $end;
 
+if (!is_int($start) || !is_int($end)) {
+    echo "Error. Both values should be numbers.";
+    return;
+}
+
+
+if ($start > $end) {
+    echo "Error. Start number should be smaller that end number.";
+    return;
+}
+
+if ($start < 0 || $end < 0) {
+    echo "Error. Both numbers should be positive.";
+    return;
+}
+
+
+function get_first_monday($year) {
+    for ($day=1; $day<=7; $day++){
+        $date = mktime(0, 0, 0, 1, $day, $year);
+        if (date("D", $date) === "Mon")
+            return $date;
+    }
+}
+
+for ($year = $start; $year <= $end; $year++){
+    echo date("d-m-Y", get_first_monday($year))."\n";
+}
 ?>
+
