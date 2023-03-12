6cd45df059fafb6767e8b40afaec676d17e88d6c
Kristofers Solo <kristians.cagulis@proton.me>
2023-03-12 19:44:46 +0200

    completed subtask 1

--- lab2/task1/index.php
+++ lab2/task1/index.php
@@ -4,12 +4,22 @@ require_once "./config.php";
 //TODO: set up Mysql connection;
 $DB = new mysqli(Config::$DBHOST, Config::$DBUSER, Config::$DPASSWORD, Config::$DBNAME);
 
+
 //TODO: Fill the array of manufacturer IDs and titles (e.g. "33" => "Alfa Romeo")
 $manufacturers = array();
+$manufacturers_handle = $DB->query("select id, title from manufacturers order by title");
+
+while ($row = $manufacturers_handle->fetch_assoc()) {
+    $manufacturers[$row["id"]] = $row["title"];
+}
 
 //TODO: Fill the array of color IDs and titles (e.g. "19" => "Tumši pelēka" (dark grey)) 
 $colors = array();
+$colors_handle = $DB->query("select id, color from manufacturers order by title");
 
+while ($row = $colors_handle->fetch_assoc()) {
+    $colors[$row["id"]] = $row["color"];
+}
 
 //TODO: collect and sanitize the current inputs from GET data
 $year = "";
